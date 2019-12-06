9d95ba24d46a673999b95d9bf612f2fbab7d99de
Blittle22 <brandonlittle22@gmail.com>
2019-12-06 12:39:18 -0700

    new main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-#include "grid.h"
-#include "row.h"
-#include "tile.h"
 
-int main(){
-	
-	Grid champ();
+using namespace std;
 
+int main()
+{
+    int columns = 9;
+    int rows = 9;
+    int array[rows][columns];
+    
+    srand(time(NULL));
+    for(int i=0; i < rows; i++){
+        for(int j=0; j < columns; j++){
+            int random = rand() % 9 + 1;
+            array[i][j] = random;
+            cout<<array[i][j];
+        }
+        cout<<endl;
+    }
 
-
-
-
-
-	return 0;
+    return 0;
 }
